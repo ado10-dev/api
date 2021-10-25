4e14a4111eef2850a139324ebddbb493111cd190
Alex <iamalexchip@gmail.com>
2021-10-25 13:00:32 +0200

    basic setup

---
+++ database/migrations/2021_10_25_064051_create_matches_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('matches', function (Blueprint $table) {
+            $table->id();
+            $table->integer('community_id');
+            $table->integer('tournament_id')->nullable();
+            $table->integer('team_a_id');
+            $table->integer('team_a_score');
+            $table->integer('team_b_id');
+            $table->integer('team_b_score');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('matches');
+    }
+}
